233534dc85411f8062e275813b32f24ebb5a877a
jeroen <jenyckee@MacBook-Pro-de-jeroen.local>
2020-03-11 13:05:26 +0100

    clean up

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
-
 import Layout from '../../components/Layout'
-import BlogRoll from '../../components/BlogRoll'
 
 export default class BlogIndexPage extends React.Component {
   render() {
@@ -25,13 +23,6 @@ export default class BlogIndexPage extends React.Component {
             Latest Stories
           </h1>
         </div>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <BlogRoll />
-            </div>
-          </div>
-        </section>
       </Layout>
     )
   }
