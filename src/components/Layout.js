c1457c1444d36bae7faa0065e3df0c60e73bbdc7
jeroen <jenyckee@MacBook-Pro-de-jeroen.local>
2020-03-11 12:57:13 +0100

    clean

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,32 +14,17 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
         <link
           rel="icon"
           type="image/png"
           href={`${withPrefix('/')}img/favicon.ico`}
           sizes="32x32"
         />
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
-        />
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta
-          property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
-        />
       </Helmet>
       <Navbar />
       <div>{children}</div>
